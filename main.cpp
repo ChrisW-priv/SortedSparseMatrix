476e0aa02bbb56b33bb107be1491f024345864b7
ChrisW-priv <chris.watras@gmail.com>
2022-05-08 17:31:33 +0200

    project refactored, Project use as library made easy

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "SortedSparseMatrix.h"
-
-int main() {
-    SortedSparseMatrix matrix{10, 30};
-    matrix.import_relations_from_file("../generic_relations.txt");
-
-    std::cout << matrix;
-}
